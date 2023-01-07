a9460ff8ee16fd8c6c31c5662466395f0966a3da
wzs <wzs@qq.com>
2023-01-07 13:36:59 +0800

    加入redis，缓存验证码和菜品

---
+++ src/main/java/com/atwzs/reggie/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.atwzs.reggie.config;
+
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+* @description: springboot默认是配置的，但是我们要用到另一个功能key序列化器
+**/
+@Configuration
+public class RedisConfig extends CachingConfigurerSupport {
+    @Bean
+    public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<Object,Object> redisTemplate = new RedisTemplate<>();
+        //默认的Key序列化器为: JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());   //方便图形化界面
+        redisTemplate.setConnectionFactory( connectionFactory) ;
+        return redisTemplate;
+    }
+}
